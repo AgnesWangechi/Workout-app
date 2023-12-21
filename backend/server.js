5d45f917925de3616ec5254c4b5e74251d09d601
AgnesWangechi <engwangechi@gmail.com>
2023-12-21 18:57:10 +0300

    Update Server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 // We need to require the express package that was installed
 const express = require('express')
 
@@ -5,7 +7,19 @@ const express = require('express')
 // app is our express app
 const app = express()
 
-//Listen for requests
-app.listen(4000, () =>{
-    console.log('listening on port 4000')
+//React to the requests we get
+//get - get the request from browser
+//Create a route handler
+//req - request object that will have the request
+//res - response object used to send a response back to the browser
+
+app.get('/',(req,res) =>{
+    res.json({mssg: "Welcome to the app"}) // This is to send a response back to the browser
 })
+
+//Listen for requests
+app.listen(process.env.PORT, () =>{
+    console.log('listening on port' ,process.env.PORT)
+})
+
+
